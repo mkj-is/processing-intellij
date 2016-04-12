053fac2bd239771adbf0b0f54178590e3ab27aa8
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2016-04-12 16:35:23 +0200

    Added guidance to example and default configuration

--- src/ExampleApplet.java
+++ src/ExampleApplet.java
@@ -9,16 +9,19 @@ public class ExampleApplet extends PApplet {
 
     @Override
     public void settings() {
+        // TODO: Customize screen size and so on here
         size(200, 200);
     }
 
     @Override
     public void setup() {
+        // TODO: Your custom drawing and setup on applet start belongs here
         clear();
     }
 
     @Override
     public void draw() {
+        // TODO: Do your drawing for each frame here
         clear();
         fill(255);
         rect(50, 50, 100, 100);
